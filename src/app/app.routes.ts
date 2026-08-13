8985bbb203aa60c86d37e13bfc0c9f18b1b2f579
Lemonsszz <axelrcm777@gmail.com>
2026-08-12 20:12:35 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+    //admin
 
-export const routes: Routes = [];
+    //public
+export const routes: Routes = [
+
+
+    {
+        path: 'public',
+        loadComponent: () => import('./ui/public/container/component').then ((m) => m.PublicComponent),
+    },
+
+
+];
+
+//https://learn-uniorz.web.app/public
+//https://learn-uniorz.web.app/public/inicio
+//https://learn-uniorz.web.app/public/programas
+//https://learn-uniorz.web.app/public/programa/diplomado-en-adminstracion-de-servidroes :slug
+//https://learn-uniorz.web.app/public/iniciar-sesion
+
+
+//https://learn-uniorz.web.app/admin/panel
---
+++ src/app/ui/public/container/component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'public',
+    templateUrl: './component.html',
+    styleUrls: ['./component.css'],
+    imports: [],
+})
+export class PublicComponent{}
